0cd113f77d9f61efe8769fd5be53201203182f78
SaraDonzellini <saradonzie@gmail.com>
2023-01-10 16:20:19 +0100

    implemented comics list

---
+++ src/components/AppCard.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  props : [
+          'thumbnail',
+          'price',
+          'series',
+          'type'
+  ]
+}
+</script>
+
+<template>
+  <div>
+    <div class="img-wrapper">
+    <img :src="thumbnail">
+
+    </div>
+    <p>{{ series }}</p>
+  </div>
+</template>
+
+<style lang="scss">
+    .img-wrapper{
+    margin-top: 1rem;
+    height: 192px;
+    overflow: hidden;
+  }
+  p{
+    text-transform: uppercase;
+    padding-top: 1rem;
+  }
+
+</style>
